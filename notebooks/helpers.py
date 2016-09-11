c7ebdc6ea98c474257bc936e569f1071cdb7f414
Peter Baumgartner <petermbaumgartner@gmail.com>
2016-09-11 17:57:04 -0400

    community detection

---
+++ notebooks/helpers.py
@@ -0,0 +1,32 @@
+import networkx as nx
+from seaborn import color_palette, set_style, palplot
+
+
+def create_color_map(G, attribute, seaborn_palette="colorblind"):
+
+    attributes = [G.node[label][attribute] for label in G.nodes()]
+
+    # get the set of possible attributes
+    attributes_unique = list(set(attributes))
+    num_values = len(attributes_unique)
+
+    # generate color palette from seaborn
+    palette = color_palette(seaborn_palette, num_values).as_hex()
+
+    # create a mapping of attribute to color
+    color_map = dict(zip(attributes_unique, palette))
+
+    # map the attribute for each node to the color it represents
+    node_colors = [color_map[attribute] for attribute in attributes]
+
+    return node_colors, color_map, palette
+
+def map_communities(G, ga_communities):
+    community_map = {}
+    for node in G.nodes():
+        for i, comm in enumerate(ga_communities):
+            if node in comm:
+                community_map[node] = i
+        if community_map.get(node, None) is None:
+            community_map[node] = None
+    return community_map
